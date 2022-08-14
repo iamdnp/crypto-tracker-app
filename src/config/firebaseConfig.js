a7596648693ac697101433a0180ebd8372d872cf
iamdnp <iamshibu27@gmail.com>
2022-08-14 18:40:13 +0530

    second commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCvcgZ_GYA8HmkdQ_2t3_pOirGXy7Ok30Q",
+  authDomain: "crypto-tracker-d4496.firebaseapp.com",
+  projectId: "crypto-tracker-d4496",
+  storageBucket: "crypto-tracker-d4496.appspot.com",
+  messagingSenderId: "431091594396",
+  appId: "1:431091594396:web:a38e2773ee1cee282d2959"
+};
+
+
+export default firebaseConfig;
